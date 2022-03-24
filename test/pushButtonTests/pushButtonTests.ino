a9ebc5c3ba04211f356bc555c8edd4a4fc1910bd
JinU(지누) <73889507+jinu12@users.noreply.github.com>
2022-03-24 17:42:13 +0900

    update

--- test/pushButtonTests/pushButtonTests.ino
+++ test/pushButtonTests/pushButtonTests.ino
@@ -1,10 +1,9 @@
-
 int push_btn = 12;
-int btn_state = 0; //버튼의 혀냊 상태 값을 저장하는 변수 - 0, 1
+int btn_state =0;
 void setup() {
   Serial.begin(9600);
   pinMode(push_btn,INPUT);
-  int count = 0;  
+
 }
 
 void loop() {
@@ -12,4 +11,5 @@ void loop() {
   // 입력핀으로 설정한 12번 핀읜 입력된 현재 값을 읽어서 btn_state에 저장하기
   btn_state = digitalRead(push_btn);
   Serial.println(btn_state);
-  }
+
+}
--- test/push_btn_led_exam1/push_btn_led_exam1.ino
+++ test/push_btn_led_exam1/push_btn_led_exam1.ino
@@ -1,32 +1,30 @@
 int led_pin = 8;
 int push_btn = 12;
-int ledset[1] = {0};
+int led_state = 0;
 int btn_state = 0; //버튼의 혀냊 상태 값을 저장하는 변수 - 0, 1
 void setup() {
   Serial.begin(9600);
   pinMode(push_btn,INPUT);
-  pinMode(led_pin,OUTPUT);
-  int count = 0;  
+  pinMode(led_pin,OUTPUT);  
 }
 
 void loop() {
-  int count;
-  delay(1000);
+  delay(500);
   // 입력핀으로 설정한 12번 핀읜 입력된 현재 값을 읽어서 btn_state에 저장하기
   btn_state = digitalRead(push_btn);
 //  Serial.println(btn_state);
-  Serial.println(count);
+  Serial.println(btn_state);
   if(btn_state == HIGH) {
-    ledset[0] = 1;
-    count = count +1;
-  }
-  if(count % 2 == 0) {
-    ledset[0] = 0;
-  }
-  if(ledset[0] == 1) {
-    count = count + 1;
-    digitalWrite(led_pin,HIGH);
-  }else {
-    digitalWrite(led_pin,LOW);
+    if(led_state == 0){
+      digitalWrite(led_pin,HIGH);
+      led_state = 1;// led의 상태값을 다시 설정
+      Serial.println("led_on:");
+      
+    }else {
+      digitalWrite(led_pin,LOW);
+      led_state = 0;
+      Serial.println("led_off:");
+      
+    }
   }
 }
