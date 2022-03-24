58a8537f359cd680a39ee66a9c6dabbc48029d5f
JinU(지누) <73889507+jinu12@users.noreply.github.com>
2022-03-24 14:23:17 +0900

    led push update

---
+++ test/push_btn_led_exam1/push_btn_led_exam1.ino
@@ -0,0 +1,32 @@
+int led_pin = 8;
+int push_btn = 12;
+int ledset[1] = {0};
+int btn_state = 0; //버튼의 혀냊 상태 값을 저장하는 변수 - 0, 1
+void setup() {
+  Serial.begin(9600);
+  pinMode(push_btn,INPUT);
+  pinMode(led_pin,OUTPUT);
+  int count = 0;  
+}
+
+void loop() {
+  int count;
+  delay(1000);
+  // 입력핀으로 설정한 12번 핀읜 입력된 현재 값을 읽어서 btn_state에 저장하기
+  btn_state = digitalRead(push_btn);
+//  Serial.println(btn_state);
+  Serial.println(count);
+  if(btn_state == HIGH) {
+    ledset[0] = 1;
+    count = count +1;
+  }
+  if(count % 2 == 0) {
+    ledset[0] = 0;
+  }
+  if(ledset[0] == 1) {
+    count = count + 1;
+    digitalWrite(led_pin,HIGH);
+  }else {
+    digitalWrite(led_pin,LOW);
+  }
+}
